5691838319c9e7fdb0e89216c2d3dc783013b779
ouroborosLi <870763856@qq.com>
2021-04-11 18:28:54 +0800

    Site updated: 2021-04-11 18:28:54

--- js/utils.js
+++ js/utils.js
@@ -7,7 +7,7 @@
       return function () {
         var context = this;
         var args = arguments;
-        if (timeout) clearTimeout(timeout);
+        timeout && clearTimeout(timeout);
         if (immediate) {
           var callNow = !timeout;
           timeout = setTimeout(function () {
@@ -69,7 +69,99 @@
     },
     isDesktop: function () {
       return !this.isTablet() && !this.isMobile();
+    },
+    isDuringDate: function (beginDateStr, endDateStr) {
+      var curDate = new Date(),
+        beginDate = new Date(beginDateStr),
+        endDate = new Date(endDateStr);
+      if (curDate >= beginDate && curDate <= endDate) {
+        return true;
+      }
+      return false;
+    },
+    bindKeyup: function (code, fn) {
+      $(document).keyup(function (e) {
+        var key = e.which || e.keyCode;;
+        if (key == code) {
+          fn();
+        }
+      });
+    }
+  }
+
+  ZHAOO.zui = {
+    message: function ({ text, type, delay }) {
+      var message = '<div class="zui-message ' + (type || "info") + '"><p>' + text + '</p></div>';
+      $("body").append(message);
+      var e = $(".zui-message");
+      e.ready(function () {
+        e.addClass("in");
+        setTimeout(function () {
+          e.removeClass("in");
+          e.on("transitionend webkitTransitionEnd", function () {
+            $(this).remove();
+          });
+        }, delay || 3000);
+      });
+    },
+    notification: function ({ title, content, type, delay }) {
+      var storage = JSON.parse(localStorage.getItem("notification-closed"));
+      if (storage && storage.indexOf(title) >= 0) return;
+      var notification = '<div class="zui-notification ' + (type || "info") + '"><span>' + title + '</span><p>' + content + '</p><i class="j-notification-close iconfont iconbaseline-close-px"></i></div>';
+      $("body").append(notification);
+      var e = $(".zui-notification");
+      var close = $(".j-notification-close");
+      e.ready(function () {
+        e.addClass("in");
+        setTimeout(function () {
+          e.removeClass("in");
+          e.on("transitionend webkitTransitionEnd", function () {
+            $(this).remove();
+          });
+        }, delay || 3000);
+        close.on("click", function () {
+          e.removeClass("in");
+          if (storage) {
+            (storage.indexOf(title) < 0) && localStorage.setItem("notification-closed", JSON.stringify(storage.concat(title)));
+          } else {
+            localStorage.setItem("notification-closed", JSON.stringify([title]));
+          }
+        });
+      });
     }
   }
 
 })(jQuery);
+
+class AsyncLimit {
+  constructor(limit) {
+    this.limit = Number(limit) || 2;
+    this.pool = [];
+    this.current = 0;
+  }
+
+  async run(fn) {
+    if (!fn || typeof fn !== 'function') {
+      throw new Error('Function error.');
+    }
+    if (this.current >= this.limit) {
+      await new Promise(resolve => this.pool.push(resolve));
+    }
+    return this._handleRun(fn);
+  }
+
+  async _handleRun(fn) {
+    this.current++;
+    try {
+      return await fn();
+    } catch (err) {
+      return Promise.reject(err);
+    } finally {
+      this.current--;
+      if (this.pool.length) {
+        this.pool[0]();
+        this.pool.shift();
+      }
+    }
+  }
+}
